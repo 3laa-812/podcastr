9dd3127e2e1a5d4e3022e1b398c2df6e46c26a9d
3laa <3laa.r.812@gmail.com>
2025-06-26 19:03:15 +0300

    handle-routing-and-folder-structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" flex flex-col gap-2">
+      <section className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">trending podcasts</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
